112154fafa16468b424f7954fd70ed8fb8c93815
Jonny Goff-White <jhg14@ic.ac.uk>
2016-05-19 17:52:15 +0100

    Refactoring and commenting

--- src/main/Board.java
+++ src/main/Board.java
@@ -16,6 +16,8 @@ public class Board {
 
     private static final char PLACEHOLDER = '%';
     private static final char DOOR = 'D';
+    private static final char EMPTY = '_';
+    private static final char WALL = 'w';
 
     private int numberOfRooms;
     private List<List<ReplacementNode>> groups;
@@ -30,6 +32,9 @@ public class Board {
     private int x;
     private int y;
 
+    /*
+     * Constructor for the Board Class.
+     */
     public Board(int x, int y){
         this.x = x;
         this.y = y;
@@ -44,12 +49,19 @@ public class Board {
         numberOfDoorsInEachGroup = new ArrayList<>();
     }
 
+    /*
+     * Initialises the 2D array of chars, tiles to empty space.
+     */
     private void initBoard(){
         for (int j = 0; j < tiles.length; j++){
-            Arrays.fill(tiles[j], '_');
+            Arrays.fill(tiles[j], EMPTY);
         }
     }
 
+    /*
+     * Intialises the boolean array, doors, that determines whether there
+     * is a door at a given location - doors lie on top of walls
+     */
     private void initDoors() {
         for (Boolean[] row: doors) {
             for (int j = 0; j < y; j++) {
@@ -58,7 +70,10 @@ public class Board {
         }
     }
 
-    //Account for out of board size accesses
+    /*
+     * Delegates to the respective method call, given an enum which
+     * identifies what command the user has input.
+     */
     public Board applyInstruction (Instruction i) {
         int[] args = i.getArguments();
         switch (i.getCommand()) {
@@ -74,11 +89,15 @@ public class Board {
         return null;
     }
 
+    /*
+     * Adds a room at the given location, and of the given height and width.
+     * The room is bounded by walls.
+     */
     private Board add (int x, int y, int width, int height) {
         for (int i = x; i < x+width; i++) {
             for (int j = y; j < y+height; j++) {
                 if (i == x || j == y || i == (x+width-1) || j == (y+height-1)){
-                    tiles[i][j] = 'w';
+                    tiles[i][j] = WALL;
                 }
             }
         }
@@ -86,10 +105,13 @@ public class Board {
         return this;
     }
 
+    /*
+     * Removes the wall, and doors contained within the given area.
+     */
     private Board remove(int x, int y, int width, int height) {
         for (int i = x; i < x+width; i++) {
             for (int j = y; j < y+height; j++) {
-                tiles[i][j] = '_';
+                tiles[i][j] = EMPTY;
                 if (doors[i][j]) doors[i][j] = false;
             }
         }
@@ -97,13 +119,23 @@ public class Board {
         return this;
     }
 
+    /*
+     * Adds a door to the given location in the array,
+     * as long as the location contains a wall.
+     */
     private Board addDoor(int x, int y) {
 
-        doors[x][y] = true;
-        updateNumberOfDoors();
+        if (tiles[x][y] == WALL) {
+            doors[x][y] = true;
+            updateNumberOfDoors();
+        }
         return this;
     }
 
+    /*
+     * Calls the relevant methods to update the board
+     * after changes have been applied e.g. via an add.
+     */
     private void update() {
         markAllButWallsAsTbd();
         removeInvalidTbdTiles();
@@ -112,12 +144,18 @@ public class Board {
         updateNumberOfDoors();
     }
 
+    /*
+     * Populates the array with characters that are valid room identifiers.
+     */
     private void populateAlphabet() {
         for (int letter = ALPHABET_START_DEC; letter <= ALPHABET_END_DEC; letter++) {
             alphabet[letter-ALPHABET_START_DEC] = (char) letter;
         }
     }
 
+    /*
+     * Retrieves an unused letter to be used for identifying a room.
+     */
     private char getLetter(){
         for (int i = 0; i < alphaBitmap.length; i++) {
             if (!alphaBitmap[i]) {
@@ -128,17 +166,31 @@ public class Board {
         return '#';
     }
 
+    /*
+     * Frees all the used letters, for use after a change
+     * of the board, so that the rooms can be reallocated letters.
+     */
     private void freeLetters() {
         for (int i = 0; i < alphaBitmap.length; i++) {
             alphaBitmap[i] = false;
         }
         lettersInUse.clear();
+
+        //D is an invalid room identifier.
+        alphaBitmap[4] = true;
     }
 
+    /*
+     * After the board has been mutated, with addition
+     * or removal of walls, this method will set all space
+     * except for walls as a PLACEHOLDER.
+     * After this, the rooms and empty space can be recognised,
+     * and allocated letters accordingly.
+     */
     private void markAllButWallsAsTbd() {
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
-                if (tiles[i][j] != 'w') {
+                if (tiles[i][j] != WALL) {
                     tiles[i][j] = PLACEHOLDER;
                 }
             }
@@ -147,17 +199,17 @@ public class Board {
 
 
     /* Every space has been marked as PLACEHOLDER
-       1. Anything that is an edge, mark '_'
-       2. Anything that is next to first '_', make '_'
-           2.1. Until you reach first 'w'
-       3. Repeat steps 2 & 3 until no change
+     * 1. Anything that is an edge, mark EMPTY
+     * 2. Anything that is next to EMPTY, make EMPTY
+     *     2.1. Unless WALL
+     * 3. Repeat steps 2 & 3 until no change
      */
     public void removeInvalidTbdTiles() {
 
         //Replace edge tiles with '_'
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
-                if (isEdge(i, j) && (tiles[i][j] != 'w')) tiles[i][j] = '_';
+                if (isEdge(i, j) && (tiles[i][j] != WALL)) tiles[i][j] = EMPTY;
             }
         }
 
@@ -168,18 +220,18 @@ public class Board {
             change = 0;
             for (int i = 0; i < x; i++) {
                 for (int j = 0; j < y; j++) {
-                    if (tiles[i][j] != 'w' && tiles[i][j] != '_') {
+                    if (tiles[i][j] != WALL && tiles[i][j] != EMPTY) {
                         List<Character> adj = getAdjacentTilesChar(i, j, tiles);
                         boolean adjacentToEmpty = false;
 
                         for (Character c : adj) {
-                            if (c.charValue() == '_') {
+                            if (c.charValue() == EMPTY) {
                                 adjacentToEmpty = true;
                             }
                         }
 
                         if (adjacentToEmpty) {
-                            tiles[i][j] = '_';
+                            tiles[i][j] = EMPTY;
                             change++;
                         }
                     }
@@ -189,8 +241,14 @@ public class Board {
 
     }
 
+    /*
+     * Detects distinct enclosed areas, groups them together,
+     * and stores them in the field 'groups'.
+     * From here, letters can be allocated, doors counted etc.
+     */
     private void detectEnclosure() {
 
+        //Make a new Replacement Node for each PLACEHOLDER
         ReplacementNode[][] nodes = new ReplacementNode[x][y];
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
@@ -200,6 +258,11 @@ public class Board {
             }
         }
 
+        //Choose the larger dimension of the two - this is the number
+        //of times we need to iterate.
+        //For each of teh Replacement Nodes, merge their groups, giving
+        //them information about each other, until each node in a given group
+        //holds identical information to each other node in the same group.
         int k = (x > y) ? x : y;
         do {
             for (int i = 0; i < x; i++) {
@@ -235,7 +298,6 @@ public class Board {
             }
         }
         this.groups = groups;
-        System.out.println(groups);
 
         //Convert the list of list of replacement nodes into first list of list of indices,
         //and at each, store the letter assigned and the number of doors in the area.
